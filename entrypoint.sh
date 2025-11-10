0ca63bd66f2c5c1450693ce9162742e178ab076d
Robotics-Living-Lab <roboticslivinglab@mmu.ac.uk>
2025-11-10 15:26:22 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Source ROS2 and workspace
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /turtlebot3_ws/install/setup.bash
+
+# Set environment variables
+export TURTLEBOT3_MODEL=$TURTLEBOT3_MODEL
+export ROS_DOMAIN_ID=$ROS_DOMAIN_ID
+
+# Launch a shell (or you can auto‐launch bringup etc)
+exec "$@"
